b0e9c74d4f66b2881784e5d3c936532c9d221395
Ericcs10 <eric.camargo.silva@gmail.com>
2025-05-07 10:28:39 -0300

    fix:ajuste no arquivo main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,3 +12,7 @@ async def startup_db_client():
 @app.get("/")
 async def hello_world():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
