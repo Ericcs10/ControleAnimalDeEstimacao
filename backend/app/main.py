198320b97f8f0e270f840a3d95630aa828577298
Ericcs10 <eric.camargo.silva@gmail.com>
2025-05-07 09:29:58 -0300

    feat: alterações main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient(os.getenv("MONGODB_URL", "mongodb://root:example@mongo:27017"))
+    app.mongodb = app.mongodb_client[os.getenv("MONGO_DB", "petdb")]
 
 @app.get("/")
 async def hello_world():
