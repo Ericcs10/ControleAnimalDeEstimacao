5141a56402420eb623a5d965dd77b05c9fdaac22
Ericcs10 <eric.camargo.silva@gmail.com>
2025-06-17 11:44:45 -0300

    feat(frontend) ajustes no docker, package.json e src, alem do backend funcionando agora

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import CadastroUsuario from './pages/CadastroUsuario';
-
 function App() {
   return (
-    <div className="App">
-      <CadastroUsuario />
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Controle de Animais
+      </h1>
+      <p className="mt-2">Frontend funcionando com Vite + React + Tailwind 🚀</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
