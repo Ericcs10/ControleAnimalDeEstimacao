7a7dffb9b46efd34f48841b31095ca7d6fb4074c
Ericcs10 <eric.camargo.silva@gmail.com>
2025-06-24 12:12:27 -0300

    refactor(backend) deixando o config.py mais limpo

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,16 +2,15 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    MONGO_HOST: str = "localhost"
+    MONGO_HOST: str = "mongo"
     MONGO_PORT: int = 27017
     MONGO_USER: str = "root"
     MONGO_PASS: str = "example"
-    MONGO_DB: str = "petdb"
-    MONGODB_NAME: str = "petdb"  # 🔥 Adicionado
+    MONGO_DB: str = "petdb" 
 
     @property
-    def MONGODB_URL(self):
-        return f"mongodb://{self.MONGO_USER}:{self.MONGO_PASS}@{self.MONGO_HOST}:{self.MONGO_PORT}"
+    def MONGODB_URL(self) -> str:
+        return f"mongodb://{self.MONGO_USER}:{self.MONGO_PASS}@{self.MONGO_HOST}:{self.MONGO_PORT}/{self.MONGO_DB}?authSource=admin"
 
     class Config:
         env_file = ".env.docker"
