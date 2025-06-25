a8920fda6d8fb1476a5789511a303cbb381eefb8
Ericcs10 <eric.camargo.silva@gmail.com>
2025-06-25 09:50:36 -0300

    fix(backend) ajustes no requirements.txt, arrumando erros de importação de um arquivo para o outro

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,11 +6,11 @@ class Settings(BaseSettings):
     MONGO_PORT: int = 27017
     MONGO_USER: str = "root"
     MONGO_PASS: str = "example"
-    MONGO_DB: str = "petdb" 
+    MONGO_DB: str = "petdb"
 
     @property
-    def MONGODB_URL(self) -> str:
-        return f"mongodb://{self.MONGO_USER}:{self.MONGO_PASS}@{self.MONGO_HOST}:{self.MONGO_PORT}/{self.MONGO_DB}?authSource=admin"
+    def MONGODB_URL(self):
+        return f"mongodb://{self.MONGO_USER}:{self.MONGO_PASS}@{self.MONGO_HOST}:{self.MONGO_PORT}"
 
     class Config:
         env_file = ".env.docker"
--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -2,4 +2,4 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from app.core.config import settings
 
 client = AsyncIOMotorClient(settings.MONGODB_URL)
-db = client[settings.MONGODB_NAME]
+db = client[settings.MONGO_DB]
